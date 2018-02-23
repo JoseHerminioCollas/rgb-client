5c12e02e2e907b775a8f8bbb5a31beae2ac67cca
Jose Collas <joseherminiocollas@goatstone.com>
2018-02-23 13:34:02 -0800

    more refactor

--- src/goatstone/components/effect-select.js
+++ src/goatstone/components/effect-select.js
@@ -5,47 +5,46 @@ import { h, input, label } from '@cycle/dom'
 
 function EffectSelect(sources) {
   const inputClassName = '.effect'
+  // TODO should copy be another stream?????
   const copy = sources.copy
   const effects = [copy.effect.options.glow, copy.effect.options.chase, copy.effect.options.redBlue]
+  const startValue = null
   const newValue$ = sources.DOM
     .select('[name=effect-choice]')
     .events('change')
     .map(ev => ev.target.value)
-    .startWith(0)
-
+    .startWith(startValue)
   const state$ = newValue$
     .map(val => ({
       value: val,
       displayValue: effects[val]
     }))
     .remember()
-
-  // should copy be another stream?????
-  const vdom$ = state$.map(({ displayValue }) =>
+  const vdom$ = state$.map(({ value, displayValue }) =>
     h('article', {}, [
       h('h3', { style: { fontWeight: 900 } }, [copy.effect.title,
-        h('span', { style: { color: 'black', fontSize: '.7em' } }, [' ', displayValue]),
+        h('span', { style: { color: 'black', fontSize: '.7em', fontWeight: 'normal' } }, [' ', displayValue]),
       ]),
       label('', {}, [
         copy.effect.options.glow,
-        input(inputClassName, { attrs: { type: 'radio', name: 'effect-choice', value: 0, selected: true } }),
+        input(inputClassName, {
+          attrs: { type: 'radio', name: 'effect-choice', value: 0, checked: value === 0 } }),
       ]),
       label('', {}, [
         copy.effect.options.chase,
-        input(inputClassName, { attrs: { type: 'radio', name: 'effect-choice', value: 1 } }),
+        input(inputClassName, {
+          attrs: { type: 'radio', name: 'effect-choice', value: 1, checked: value === 1 } }),
       ]),
       label('', {}, [
         copy.effect.options.redBlue,
-        input(inputClassName, { attrs: { type: 'radio', name: 'effect-choice', value: 2 } }),
+        input(inputClassName, {
+          attrs: { type: 'radio', name: 'effect-choice', value: 2, checked: value === 2 } }),
       ]),
     ])
   )
-
-  const sinks = {
+  return {
     val: newValue$,
     DOM: vdom$
   }
-  return sinks
 }
-
 export default EffectSelect
