0991f9dd714426945e1b36a6c6ecf53b6343878c
Jose Collas <joseherminiocollas@goatstone.com>
2018-03-07 09:55:00 -0800

    Add sliders to colorPicker, remove sliders from main.

--- src/goatstone/components/color-picker.js
+++ src/goatstone/components/color-picker.js
@@ -4,20 +4,59 @@ the 3 slider values will be compromise a single color value
 */
 import xs from 'xstream'
 import { h } from '@cycle/dom'
+import ColorSlider from './color-slider'
 
 function ColorPicker(sources) {
-  const value$ = xs.of(0)
-  const state$ = xs.of({
-    value: 0,
-    red: sources.initValues[0],
-    message: 'msg'
-  })
-  const vdom$ = state$.map(({ value, message, red }) =>
-    h('article', {}, ['color picker', String(value), message, red]))
+  // color components need distinct selectors (DOM) in order to create distinct values
+  const redColorSlider = ColorSlider(
+    { DOM: sources.DOM,
+      copy: { label: `Red` },
+      selector: 'red-select' })
+  const redColorSliderVDOM$ = redColorSlider.DOM
+  const redColorSliderValue$ = redColorSlider.value
+
+  const greenColorSlider = ColorSlider(
+    { DOM: sources.DOM,
+      copy: { label: 'Green' },
+      selector: 'green-select' })
+  const greenColorSliderVDOM$ = greenColorSlider.DOM
+  const greenColorSliderValue$ = greenColorSlider.value
+
+  const blueColorSlider = ColorSlider(
+    { DOM: sources.DOM,
+      copy: { label: 'Blue' },
+      selector: 'blue-select' })
+  const blueColorSliderVDOM$ = blueColorSlider.DOM
+  const blueColorSliderValue$ = blueColorSlider.value
+
+  const state$ = xs.combine(
+    redColorSliderValue$, greenColorSliderValue$, blueColorSliderValue$)
+    .map(([red, green, blue]) =>
+      [red, green, blue])
+
+  const vdom$ = xs.combine(
+    state$,
+    redColorSliderVDOM$,
+    greenColorSliderVDOM$,
+    blueColorSliderVDOM$
+  ).map((
+    [
+      state,
+      redColorSliderVDOM,
+      greenColorSliderVDOM,
+      blueColorSliderVDOM
+    ]) =>
+    h('article', {}, [
+      'color picker',
+      redColorSliderVDOM,
+      greenColorSliderVDOM,
+      blueColorSliderVDOM,
+      JSON.stringify(state)
+    ]))
 
   return {
     DOM: vdom$,
-    value: value$
+    value: xs.of(0)
   }
 }
 
--- src/goatstone/main.js
+++ src/goatstone/main.js
@@ -5,7 +5,6 @@ import {
 } from '@cycle/dom'
 import bootstrap from '../bootstrap'
 import copy from './copy'
-import ColorSlider from './components/color-slider'
 import EffectSelect from './components/effect-select'
 import ComponentOne from './components/component-one'
 import ColorPicker from './components/color-picker'
@@ -22,28 +21,6 @@ function main(sources) {
   const colorPickerVDOM$ = colorPicker.DOM
   const colorPickerValue$ = colorPicker.value
 
-  // color components need distinct selectors (DOM) in order to create distinct values
-  const redColorSlider = ColorSlider(
-    { DOM: sources.DOM,
-      copy: { label: `Red` },
-      selector: 'red-select' })
-  const redColorSliderVDOM$ = redColorSlider.DOM
-  const redColorSliderValue$ = redColorSlider.value
-
-  const greenColorSlider = ColorSlider(
-    { DOM: sources.DOM,
-      copy: { label: 'Green' },
-      selector: 'green-select' })
-  const greenColorSliderVDOM$ = greenColorSlider.DOM
-  const greenColorSliderValue$ = greenColorSlider.value
-
-  const blueColorSlider = ColorSlider(
-    { DOM: sources.DOM,
-      copy: { label: 'Blue' },
-      selector: 'blue-select' })
-  const blueColorSliderVDOM$ = blueColorSlider.DOM
-  const blueColorSliderValue$ = blueColorSlider.value
-
   const effectSelect = EffectSelect({ DOM: sources.DOM, copy })
   const effectSelectVDOM$ = effectSelect.DOM
   const effectSelectValue$ = effectSelect.val
@@ -53,27 +30,28 @@ function main(sources) {
   const componentOneValue$ = componentOne.value
 
   const state$ = xs.combine(
-    redColorSliderValue$, effectSelectValue$,
-    componentOneValue$, greenColorSliderValue$, blueColorSliderValue$, colorPickerValue$)
-    .map(([redColorSliderValue, effectSelectValue,
-      componentOneValue, greenColorSliderValue, blueColorSliderValue, colorPickerValue]) =>
-      [redColorSliderValue, effectSelectValue,
-        componentOneValue, greenColorSliderValue, blueColorSliderValue, colorPickerValue])
+    effectSelectValue$,
+    componentOneValue$, colorPickerValue$)
+    .map(([effectSelectValue,
+      componentOneValue, colorPickerValue]) =>
+      [effectSelectValue,
+        componentOneValue, colorPickerValue])
 
   const vdom$ = xs.combine(
-    state$, redColorSliderVDOM$, effectSelectVDOM$,
-    componentOneVDOM$, greenColorSliderVDOM$, blueColorSliderVDOM$, colorPickerVDOM$)
-    .map(([data, redColorSliderVDOM, effectSelectVDOM,
-      componentOneVDOM, greenColorSliderVDOM, blueColorSliderVDOM, colorPickerVDOM]) => {
+    state$, effectSelectVDOM$,
+    componentOneVDOM$, colorPickerVDOM$)
+    .map(([
+      data,
+      effectSelectVDOM,
+      componentOneVDOM,
+      colorPickerVDOM
+    ]) => {
       const displayData = JSON.stringify(data)
       return h('section',
         [
           titleVDOM,
           effectSelectVDOM,
           componentOneVDOM,
-          redColorSliderVDOM,
-          greenColorSliderVDOM,
-          blueColorSliderVDOM,
           colorPickerVDOM,
           p(`Values::  ${displayData}`)
         ])
