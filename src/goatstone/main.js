7343ce4ef87ee0f4b9b5121175f8ce41a1395846
Jose Collas <joseherminiocollas@goatstone.com>
2018-03-08 13:16:43 -0800

    Use functions to return request configuration

--- src/goatstone/main.js
+++ src/goatstone/main.js
@@ -9,54 +9,75 @@ import ComponentOne from './components/component-one'
 import ColorPicker from './components/color-picker'
 
 const titleVDOM = h('header', { style: { color: '#333' } }, copy.title)
-
+// getColorRequest getEffectRequest
+function getColorRequest({ host, name, device, colorValue }) { // colorValue
+  console.log(' dd', host, name, device, colorValue)
+  const url = `${host}/${name}/${device}/color/${colorValue}`
+  const request = {
+    url,
+    category: 'color',
+    method: 'POST'
+  }
+  request.query = colorValue
+  return request
+}
+function getEffectRequest(d) {
+  console.log(' ef', d)
+  const url = 'http://localhost:3000/rgb/light/set/red-blue'
+  const request = {
+    url,
+    category: 'effect',
+    method: 'POST'
+  }
+  request.query = ''
+  return request
+}
+const requestConfig =
+  {
+    host: 'http://localhost:3000',
+    name: 'rgb',
+    device: 'light'
+  }
 bootstrap()
 function main(sources) {
 
-  const url = 'http://localhost:3000/rgb/light/set/glow'
-  const user$ = sources.HTTP
-    .select('users')
+  const colorReqRes$ = sources.HTTP
+    .select('color')
     .flatten()
-    .map(res => `;;; ${res.text} ;;;`)
-
+    .map(res => `Color Applied:  ${res.text} `)
   const colorPicker = ColorPicker({
     DOM: sources.DOM,
     initValues: [199, 199, 199]
   })
   const colorPickerVDOM$ = colorPicker.DOM
   const colorPickerValue$ = colorPicker.value
-  const request$ = colorPickerValue$
-    .map(() =>
-      ({
-        url,
-        category: 'users',
-        method: 'POST'
-      }))
-
+  const colorRequest$ = colorPickerValue$
+    .map(colorValue => Object.assign(requestConfig, { colorValue }))
+    .map(getColorRequest)
   const effectSelect = EffectSelect({ DOM: sources.DOM, copy })
   const effectSelectVDOM$ = effectSelect.DOM
   const effectSelectValue$ = effectSelect.val
-
+  const effectRequest$ = effectSelectValue$
+    .map(getEffectRequest)
   const componentOne = ComponentOne({ DOM: sources.DOM })
   const componentOneVDOM$ = componentOne.DOM
   const componentOneValue$ = componentOne.value
-
   const state$ = xs.combine(
     effectSelectValue$,
     componentOneValue$,
     colorPickerValue$,
-    user$,
+    colorReqRes$,
   ).map(([
     effectSelectValue,
     componentOneValue,
     colorPickerValue,
-    user,
+    colorReqRes,
   ]) =>
     [
       effectSelectValue,
       componentOneValue,
       colorPickerValue,
-      user,
+      colorReqRes,
     ]).startWith([null])
 
   const vdom$ = xs.combine(
@@ -80,6 +101,7 @@ function main(sources) {
         p(`:::  ${displayData} :::`)
       ])
   })
+  const request$ = xs.merge(colorRequest$, effectRequest$)
   return { DOM: vdom$, HTTP: request$ }
 }
 const drivers = {
